236c1dc97c94d90c6915b19524da0492ad148a9d
kristoffersaltvedt <kristoffer.saltvedt@gmail.com>
2020-12-08 19:08:24 +0100

    Update to files

---
+++ Source/Noroff_Exam2021/Public/Quaternions_Rotation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Quaternions_Rotation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOROFF_EXAM2021_API UQuaternions_Rotation : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
