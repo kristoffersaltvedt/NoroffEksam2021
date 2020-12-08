133fd812461cb040baae7b4e3d0bf186ad7d00fb
kristoffersaltvedt <kristoffer.saltvedt@gmail.com>
2020-12-08 14:58:29 +0100

    ERROR: Plugin 'GitSourceControl' (referenced via default plugins) does not contain the 'GitSourceControl' module, but lists it in 'C:\Users\krist\OneDrive\Dokumenter\GitHub\NoroffEksam2021\Plugins\SRombauts-UE4GitPlugin-2c11042\GitSourceControl.uplugin'. Build failed.

---
+++ Source/Noroff_Exam2021/Quant_Rotation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Quant_Rotation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOROFF_EXAM2021_API UQuant_Rotation : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
