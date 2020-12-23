2e90e3d0de3ec46f50e624dcffad9ea58517271d
kristoffersaltvedt <65606100+kristoffersaltvedt@users.noreply.github.com>
2020-12-24 00:33:47 +0100

    Further Testing

--- Plugins/AwsDynamoDB/Source/AwsDynamoDB/Private/AwsDynamoDBTypes.cpp
+++
@@ -1,8 +0,0 @@
-/**
-* Copyright (C) 2017-2020 | eelDev (Dry Eel Development)
-*/
-
-#include "AwsDynamoDBTypes.h"
-#include "AwsDynamoDBAsync.h"
-#include "AwsDynamoDBLogging.h"
-#include "AwsDynamoDBPrivatePCH.h"
